d0d3146321944338e998571ed50743393377266f
Kevin Yves Tobias Allemann <py14avk@eduvaud.ch>
2023-05-09 14:21:16 +0200

    change

--- TEST3 SLO2023_ALLEMANN/Source.c
+++ TEST3 SLO2023_ALLEMANN/Source.c
@@ -9,17 +9,26 @@
 void main() {
 	const char* pt_message = TITRE;
 	const char tbMSG[] = { 0x53,0x4C,0x4F };
-	char tbMSGComplet[15];
+	char tbMSGComplet[15] = { ' ' };
 	int annee = 2022;
 	int8_t version = 0x33;
-	int i,a;
-	a = strlen(tbMSGComplet);
 
-	//char b;
-	//itoa(version, b, 10);
+	char st[5];
 
+	// titre
 	strcpy(tbMSGComplet, pt_message);
-	
+	// 3
+	tbMSGComplet[4] = version;
+	// espace
+	tbMSGComplet[5] = ' ';
+	// SLO
+	tbMSGComplet[6] = tbMSG[0];
+	tbMSGComplet[7] = tbMSG[1];
+	tbMSGComplet[8] = tbMSG[2];
+	// annee
+	itoa(annee, st, 10);
+	strcat(tbMSGComplet, st);
+	// printf
 	printf("%s\n", tbMSGComplet);
 
 }
