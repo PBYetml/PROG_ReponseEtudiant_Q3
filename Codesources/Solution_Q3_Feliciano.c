bbeac0d2f05c603dd00cfc79d4f2a1ed68577014
CyrilFeliciano <113518224+CyrilFeliciano@users.noreply.github.com>
2023-06-05 13:28:31 +0200

    Add files via upload

--- Codesources/Solution_Q3_Feliciano.c
+++ Codesources/Solution_Q3_Feliciano.c
@@ -5,9 +5,8 @@
 
 #define TITRE "TEST"
 
-// A effacer - juste pour demo 
 
-void main() 
+void main()
 {
 	const char* pt_message = TITRE;
 	const char tbMSG[] = { 0x53,0x4C,0x4F };
@@ -17,14 +16,15 @@ void main()
 
 
 	strcpy(tbMSGComplet, pt_message);	//Copie de la valeur de pt_message dans tbMSGComplet => "TEST"
-	
+
 	strncat(tbMSGComplet, &version, 1);	// Copie du nombre "3"	
 	tbMSGComplet[5] = ' ';				// Ajout d'un esspace dans la case n°5 de tbMSGComplet
-	
+
 	strncat(&tbMSGComplet[6], &tbMSG, sizeof(tbMSG) / sizeof(char));	// Copie du mot "SLO" stocké dans le tableau tbMSG
-	
+
 	itoa(annee, &tbMSGComplet[strlen(tbMSGComplet)], 10);				// Copie du nombre "2022" stocké dans la variable annee
 
-	
-	printf("%s \n",tbMSGComplet);		//Affichage du tableau tbMSGComplet
+	strcat(tbMSGComplet, " ");  // Ajout d'un espace à la fin de tbMSGComplet
+
+	printf("%s \n", tbMSGComplet);		//Affichage du tableau tbMSGComplet
 }
